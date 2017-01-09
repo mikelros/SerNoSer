0731f8d137e44f7c2aef3bd13966335a09114ba1
mikelros <mikelros22@gmail.com>
2017-01-09 11:05:06 +0100

    beta 0.1

--- CapaPresentacion/frmPrincipal.cs
+++ CapaPresentacion/frmPrincipal.cs
@@ -16,9 +16,34 @@ namespace CapaPresentacion
     {
         private static Negocio miNegocio = new Negocio();
         private List<Pregunta> preguntas;
+        private int puntos, fallos, aciertos;
+
         public frmPrincipal()
         {
             InitializeComponent();
+        }
+
+        private void frmPrincipal_Load(object sender, EventArgs e)
+        {
+            this.btn1.Click += new System.EventHandler(this.comprobarRespuesta);
+            this.btn2.Click += new System.EventHandler(this.comprobarRespuesta);
+            this.btn3.Click += new System.EventHandler(this.comprobarRespuesta);
+            this.btn4.Click += new System.EventHandler(this.comprobarRespuesta);
+            this.btn5.Click += new System.EventHandler(this.comprobarRespuesta);
+            this.btn6.Click += new System.EventHandler(this.comprobarRespuesta);
+            this.btn7.Click += new System.EventHandler(this.comprobarRespuesta);
+            this.btn8.Click += new System.EventHandler(this.comprobarRespuesta);
+            this.btn9.Click += new System.EventHandler(this.comprobarRespuesta);
+            this.btn10.Click += new System.EventHandler(this.comprobarRespuesta);
+            this.btn11.Click += new System.EventHandler(this.comprobarRespuesta);
+            this.btn12.Click += new System.EventHandler(this.comprobarRespuesta);
+
+            fallos = 0;
+            puntos = 0;
+
+            preguntas = miNegocio.GetPreguntas();
+            cargarPregunta();
+
         }
 
         private void cargarPregunta()
@@ -48,6 +73,7 @@ namespace CapaPresentacion
                 {
                     for (int i = 0; i < gboPreguntas.Controls.Count; i++)
                     {
+                        gboPreguntas.Controls[i].Enabled = true;
                         gboPreguntas.Controls[i].Text = "";
                         gboPreguntas.Controls[i].Tag = null;
                         gboPreguntas.Controls[i].BackColor = Color.LightGray;
@@ -56,27 +82,8 @@ namespace CapaPresentacion
                 
             } else
             {
-                MessageBox.Show("Ya no hay más preguntas.", "ATENCIÓN");
+                MessageBox.Show("Ya no hay más preguntas.", "ATENCIÓN", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
-
-        }
-
-        private void frmPrincipal_Load(object sender, EventArgs e)
-        {
-            this.btn1.Click += new System.EventHandler(this.comprobarRespuesta);
-            this.btn2.Click += new System.EventHandler(this.comprobarRespuesta);
-            this.btn3.Click += new System.EventHandler(this.comprobarRespuesta);
-            this.btn4.Click += new System.EventHandler(this.comprobarRespuesta);
-            this.btn5.Click += new System.EventHandler(this.comprobarRespuesta);
-            this.btn6.Click += new System.EventHandler(this.comprobarRespuesta);
-            this.btn7.Click += new System.EventHandler(this.comprobarRespuesta);
-            this.btn8.Click += new System.EventHandler(this.comprobarRespuesta);
-            this.btn9.Click += new System.EventHandler(this.comprobarRespuesta);
-            this.btn10.Click += new System.EventHandler(this.comprobarRespuesta);
-            this.btn11.Click += new System.EventHandler(this.comprobarRespuesta);
-            this.btn12.Click += new System.EventHandler(this.comprobarRespuesta);
-            preguntas = miNegocio.GetPreguntas();
-            cargarPregunta();
 
         }
 
@@ -87,7 +94,41 @@ namespace CapaPresentacion
 
             if (resp != null)
             {
-                btn.BackColor = resp.esCorrecta() ? Color.Green : Color.Red;
+                if (resp.esCorrecta()) {
+                    acierta(btn);
+                } else {
+                    falla(btn);
+                }
+
+                btn.Enabled = false;
+                lblPuntos.Text = puntos.ToString();
+
+            }
+        }
+
+        private void falla(Button btn)
+        {
+            btn.BackColor = Color.Red;
+            fallos++;
+            puntos /= 2;
+            if (fallos == 4)
+            {
+                MessageBox.Show("Ya has fallado todas las posibles respuestas erróneas.", "Lo siento", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                cargarPregunta();
+            }
+
+        }
+
+        private void acierta(Button btn)
+        {
+            btn.BackColor = Color.Green;
+            aciertos++;
+            puntos = puntos == 0 ? 5 : puntos * 2;
+
+            if (aciertos == 8)
+            {
+                MessageBox.Show("Ya has acertado todas las respuestas.", "Enhorabuena", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                cargarPregunta();
             }
         }
 
